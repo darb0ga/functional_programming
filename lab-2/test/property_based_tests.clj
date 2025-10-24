9b6f40c00d87aaae2e8b9a906744f290c0f91b95
clone_script <clone@example.com>
2025-10-24 16:03:00 +0300

    update generation inputs for unit_tests

--- lab-2/test/property_based_tests.clj
+++ lab-2/test/property_based_tests.clj
@@ -1,4 +1,4 @@
-(ns test
+(ns property-based-tests
   (:require [clojure.test.check.clojure-test :refer [defspec]]
             [clojure.test.check.generators :as gen]
             [clojure.test.check.properties :as prop]
